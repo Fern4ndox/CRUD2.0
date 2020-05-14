32454de5cdd212e7480dcebe55841abff3fffccb
Fern4ndox <fern4ndorecinos@gmail.com>
2020-05-14 00:20:02 -0600

    Controladores Creados

--- ProyectoFinal/Models/ViewModels/ListarEmpleados.cs
+++ ProyectoFinal/Models/ViewModels/ListarEmpleados.cs
@@ -9,7 +9,7 @@ namespace ProyectoFinal.Models.ViewModels
     {
         public int EmpleadoID { get; set; }
         public string Empleado_Nombre { get; set; }
-        public string Empleado_Apellido{ get; set; }
+        public string Empleado_Apellido { get; set; }
         public string Empleado_Direccion { get; set; }
         public int Empleado_Tel { get; set; }
         public int DeptoEmpleado_ID { get; set; }
--- ProyectoFinal/Models/ViewModels/ListarMarca.cs
+++ ProyectoFinal/Models/ViewModels/ListarMarca.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.Web.Mvc;
 
 namespace ProyectoFinal.Models.ViewModels
 {
@@ -9,5 +10,6 @@ namespace ProyectoFinal.Models.ViewModels
     {
         public int MarcaID { get; set; }
         public string Marca_Nombre { get; set; }
+
     }
 }
